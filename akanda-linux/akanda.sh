2c649b6347b67f032112bc5173e42c68cbc6dc54
Jordan Tardif <jordan@dreamhost.com>
2014-09-10 18:32:32 -0400

    Build akanda-linux from master of dreamhost/akanda-appliance

--- akanda-linux/akanda.sh
+++ akanda-linux/akanda.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 apt-get -y install git
-git clone -b linux https://github.com/dreamhost/akanda-appliance.git /tmp/akanda-appliance
+git clone https://github.com/dreamhost/akanda-appliance.git /tmp/akanda-appliance
 sh /tmp/akanda-appliance/scripts/create-akanda-raw-image.sh
