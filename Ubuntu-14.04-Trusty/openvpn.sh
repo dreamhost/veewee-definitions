dcbaa3f436989dea1bb093efbbc79023e3f88034
Jordan Tardif <jordan@dreamhost.com>
2015-02-08 22:13:48 +0000

    openvpn post install script bug fixes
    Fixing a lot of bugs in the openvpn postinstall
    script. Mainly setting up NAT for the vpn network
    so it can talk to VM's on the tenant network. Also
    cleaning the first boot script a bit and tuning
    the openvpn server.

--- Ubuntu-14.04-Trusty/openvpn.sh
+++ Ubuntu-14.04-Trusty/openvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y openvpn easy-rsa openssl ipcalc
+DEBIAN_FRONTEND=noninteractive apt-get install -y openvpn easy-rsa openssl ipcalc iptables-persistent
 mkdir /etc/openvpn/easy-rsa && mkdir /etc/openvpn/easy-rsa/keys
 cp -r /usr/share/easy-rsa/* /etc/openvpn/easy-rsa/
 
@@ -26,8 +26,25 @@ ca /etc/openvpn/ca.crt
 cert /etc/openvpn/openvpn.crt
 key /etc/openvpn/openvpn.key
 dh /etc/openvpn/dh1024.pem
+client-to-client
+keepalive 10 120
+comp-lzo
+persist-key
+persist-tun
+server 172.20.0.0 255.255.255.0
 EOF
 
+echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
+
+cat > /etc/iptables/rules.v4 << EOF
+*nat
+:PREROUTING ACCEPT [239:10258]
+:INPUT ACCEPT [236:10026]
+:OUTPUT ACCEPT [53:3555]
+:POSTROUTING ACCEPT [53:3555]
+-A POSTROUTING -s 172.20.0.0/24 -o eth0 -j MASQUERADE
+COMMIT
+EOF
 
 cat > /etc/rc.first-boot << 'EOF'
 #!/bin/bash
@@ -47,10 +64,13 @@ cp $EASY_RSA/keys/openvpn.key $EASY_RSA/keys/openvpn.crt $EASY_RSA/keys/dh1024.p
 IP_CIDR=`ip -o -f inet addr show eth0 | awk '/scope global/ {print $4}'`
 IP_NETMASK=`ipcalc -n -b $IP_CIDR |egrep ^Netmask: |awk '{print $2}'`
 IP_NETWORK=`ipcalc -n -b $IP_CIDR |egrep ^Network: |awk '{print $2}' | awk -F \/ '{print $1}'`
-TUN_GATEWAY="172.20.0.1"
 
-echo "ifconfig $TUN_GATEWAY 255.255.255.0" >> /etc/openvpn/server.conf
-echo "push \"route $IP_NETWORK $IP_NETMASK $TUN_GATEWAY\" " >> /etc/openvpn/server.conf
+echo "push \"route $IP_NETWORK $IP_NETMASK\" " >> /etc/openvpn/server.conf
+
+CLIENT_KEY_DIR="/home/dhc-user/openvpn-client-keys"
+mkdir $CLIENT_KEY_DIR && chown dhc-user.dhc-user $CLIENT_KEY_DIR
+cp $EASY_RSA/keys/client.crt $EASY_RSA/keys/client.key /etc/openvpn/ca.crt $CLIENT_KEY_DIR
+chown dhc-user.dhc-user $CLIENT_KEY_DIR/*
 
 /etc/init.d/openvpn restart && touch /etc/openvpn/.ready
 EOF
