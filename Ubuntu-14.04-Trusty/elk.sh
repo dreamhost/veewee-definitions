3b9c26a1c9927c64e9f4d77e468dd69a17be1321
Jordan Tardif <jordan@dreamhost.com>
2015-02-08 22:27:43 +0000

    elk first boot fixes
    Make sure key/crt are owned by logstash user and restart logstash
    after the key/crt are in place.

--- Ubuntu-14.04-Trusty/elk.sh
+++ Ubuntu-14.04-Trusty/elk.sh
@@ -101,6 +101,8 @@ ln -s /etc/nginx/sites-available/kibana /etc/nginx/sites-enabled/kibana
 cat > /etc/rc.first-boot << EOF
 #!/bin/bash
 openssl req -x509 -batch -nodes -newkey rsa:2048 -keyout /etc/logstash/logstash-forwarder.key -out /etc/logstash/logstash-forwarder.crt
+chown logstash.logstash /etc/logstash/logstash-forwarder.key /etc/logstash/logstash-forwarder.crt
+service logstash restart
 htpasswd -b -c /etc/nginx/conf.d/kibana.htpasswd kibana kibana
 EOF
 
