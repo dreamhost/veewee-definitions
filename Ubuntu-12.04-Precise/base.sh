92ebfc68b3fdb0fba6aa3c01243b3b0994ed69c0
David Wahlstrom <david.wahlstrom@dreamhost.com>
2015-02-24 11:24:16 -0800

    Clean out repo hashes before package installs
    This patch addresses a bug with installing some packages (namely
    nfs-common) by cleaning out repo hashes before starting package
    installs.

--- Ubuntu-12.04-Precise/base.sh
+++ Ubuntu-12.04-Precise/base.sh
@@ -1,6 +1,9 @@
 
 # Apt-install various things necessary for Ruby, guest additions,
 # etc., and remove optional things to trim down the machine.
+
+# cleanout possibly stale repo hashes
+rm -rf /var/lib/apt/lists/*
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install build-essential linux-headers-$(uname -r) #gcc
