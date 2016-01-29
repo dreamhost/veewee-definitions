72ac364eedfc6531e2a43752c930d836fad3c7f8
David Wahlstrom <david.wahlstrom@dreamhost.com>
2016-01-29 06:34:58 -0800

    Initial commit of Debian-8.3
    Adding Debian-8.3 and including a stanza disabling dhcp. which seems to
    break cloud-init on systemd images particularly when the cluster has
    fast bootups times.

---
+++ Debian-8.3/cleanup.sh
@@ -0,0 +1,35 @@
+# Clean up
+apt-get -y remove linux-headers-$(uname -r) build-essential
+apt-get -y autoremove
+apt-get -y clean
+
+# Removing leftover leases and persistent rules
+echo "cleaning up dhcp leases"
+rm /var/lib/dhcp/*
+
+# Make sure Udev doesn't block our network
+echo "cleaning up udev rules"
+rm /etc/udev/rules.d/70-persistent-net.rules
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /dev/.udev/
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
+
+echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
+echo "pre-up sleep 2" >> /etc/network/interfaces
+
+# remove auto-created /etc/hosts entry
+echo "cleaning out /etc/hosts entries"
+sed -i 's/127.0.1.1.*//' /etc/hosts
+
+# disable dhcp from eth0
+cat > /etc/network/interfaces << EOF
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet manual
+EOF
